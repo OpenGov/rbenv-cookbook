809a1c8483ddc8096016af658f80ca9a5901076c
Diego Rodriguez <drodriguez@opengov.com>
2014-11-25 17:25:47 -0800

    added support for enabling environment variables during builds

--- resources/ruby.rb
+++ resources/ruby.rb
@@ -26,6 +26,7 @@ attribute :ruby_version, :kind_of => String
 attribute :force,        :default => false
 attribute :global,       :default => false
 attribute :patch,        :default => nil
+attribute :build_envs,   :kind_of => Hash, :default => {}
 
 def initialize(*args)
   super
